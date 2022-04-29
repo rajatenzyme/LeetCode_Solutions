f2be3f62169d2fdfbb43424fad882c044f5c2c4b
Rajat Mittal <rajatmittal2012@gmail.com>
2022-04-29 20:46:38 +0530

    Time: 31 ms (59.89%), Space: 25.6 MB (8.37%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,14 +1,3 @@
-int mulRem(vector<int> nums, int k){
-    int mul = 1;
-    for(int i=0;i<nums.size();i++){
-        if(i == k)
-            continue;
-        else
-            mul*=nums[i];
-    }
-    
-    return mul;
-}
 
 
 
@@ -16,26 +5,30 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
+        int n = nums.size();
         vector<int> ans;
-        int mulAll = 1;
-        for(int i=0;i<nums.size();i++)
-            mulAll*=nums[i];
         
-        // cout<<mulAll;
-        // return ans;
+        int leftMul[n], rightMul[n];
         
-        if(mulAll == 0){
-            for(int i=0;i<nums.size();i++){
-                if(nums[i]!=0)
-                ans.push_back(0);
-                else
-                    ans.push_back(mulRem(nums,i));
-            }
-            return ans;
-        }
+        leftMul[0] = 1;
+        leftMul[1] = nums[0];
+        for(int i=2;i<n;i++)
+            leftMul[i] = leftMul[i-1] * nums[i-1];
         
-        for(int i=0;i<nums.size();i++){
-            ans.push_back(mulAll/nums[i]);
+        // for(int i=0;i<n;i++)
+        //     cout<<leftMul[i]<<" ";
+        
+        rightMul[n-1] = 1;
+        rightMul[n-2] = nums[n-1];
+        
+        for(int i=n-2;i>=0;i--)
+            rightMul[i] = rightMul[i+1] * nums[i+1];
+        
+        // for(int i=0;i<n;i++)
+        //     cout<<rightMul[i]<<" ";
+        
+        for(int i=0;i<n;i++){
+            ans.push_back(leftMul[i] * rightMul[i]);
         }
         
         return ans;

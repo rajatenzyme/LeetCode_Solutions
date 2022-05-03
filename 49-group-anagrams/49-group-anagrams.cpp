d37f016d16b340624dd583937ca0222ca09b70c8
Rajat Mittal <rajatmittal2012@gmail.com>
2022-05-03 19:57:11 +0530

    Time: 47 ms (58.56%), Space: 21.8 MB (23.21%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        vector<vector<string>>  ans;
-        
+        vector<vector<string>> ans;
         
         unordered_map<string, vector<string> > mp;
         
-        for(int i=0;i<strs.size();i++){
+        for(int i=0; i< strs.size();i++){
+            
             string str = strs[i];
             sort(str.begin(), str.end());
             mp[str].push_back(strs[i]);
@@ -19,8 +19,6 @@ public:
         }
         
         return ans;
-        
-        
         
     }
 };

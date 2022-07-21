23b860398158b1df3c8bcd995cc63e8dbb0a8bf2
Rajat Mittal <rajatmittal2012@gmail.com>
2022-07-22 01:01:48 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -10,7 +10,7 @@ public:
 //             return 1;
         
 //         return 
-//         dfs(i+1, j, m, n)+
+//         dfs(i+1, j, m, n) +
 //         dfs(i, j+1, m, n);
 //     }
     
@@ -26,18 +26,19 @@ public:
     
     int dfs(int i, int j ,int m, int n, vector<vector<int>> &dp){
         
-        
-        
         if(i>m-1 || j>n-1)
             return 0;
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
         
         if(i == m-1 && j == n-1)
             return 1;
         
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        else
-            return dp[i][j] = dfs(i+1, j, m, n,dp) + dfs(i, j+1, m, n,dp);
+        
+        
+        return dp[i][j] = dfs(i+1, j, m, n,dp) + dfs(i, j+1, m, n,dp);
     }    
     
      int uniquePaths(int m, int n) {

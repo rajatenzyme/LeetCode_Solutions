70b438ee5380342777312017c75cf13c7cd4b070
Rajat Mittal <rajatmittal2012@gmail.com>
2022-07-22 01:30:12 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -24,57 +24,57 @@ public:
         
 //     }
     
-    int dfs(int i, int j ,int m, int n, vector<vector<int>> &dp){
+//     int dfs(int i, int j ,int m, int n, vector<vector<int>> &dp){
         
-        if(i>m-1 || j>n-1)
-            return 0;
+//         if(i>m-1 || j>n-1)
+//             return 0;
         
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        
-        if(i == m-1 && j == n-1)
-            return 1;
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
         
         
+//         if(i == m-1 && j == n-1)
+//             return 1;
         
-        return dp[i][j] = dfs(i+1, j, m, n,dp) + dfs(i, j+1, m, n,dp);
-    }    
+        
+        
+//         return dp[i][j] = dfs(i+1, j, m, n,dp) + dfs(i, j+1, m, n,dp);
+//     }    
+    
+//      int uniquePaths(int m, int n) {
+        
+//          vector<vector<int> > dp( m , vector<int> (n,-1));
+         
+//          return dfs(0,0,m,n,dp);
+//      }
+         
     
      int uniquePaths(int m, int n) {
         
-         vector<vector<int> > dp( m , vector<int> (n,-1));
+         vector<vector<int> > dp( m , vector<int> (n));
          
-         return dfs(0,0,m,n,dp);
+          for(int i=0;i<m;i++){
+             for(int j=0;j<n;j++){
+                       if(i==0 || j == 0)
+                            dp[i][j] = 1;
+                        else
+                            dp[i][j] = dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                 
+             }
+         }
          
-//           for(int i=0;i<m;i++){
+//         for(int i=0;i<m;i++){
 //              for(int j=0;j<n;j++){
-//                      dp[i][j] = dp[i+1][j] + dp[i][j+1];
+//                      dp[i][j] = dp[i-1][j] + dp[i][j-1];
                  
 //              }
 //          }
          
-//          for(int i=0;i<m;i++){
-//              for(int j=0;j<n;j++){
-//                      dp[i][j] = dp[i+1][j] + dp[i][j+1];
-                 
-//              }
-//          }
-         
-         
-         
-         // for(int i=1;i<m+1;i++){
-         //     for(int j=1;j<n+1;j++){
-         //           dp[i][j] = dp[i+1][j] + dp[i][j+1];
-         //     }
-         // }
-         //return dp[m-1][n-1];
-         
-         
-         
-         
-        //return dfs(0,0,m,n,dp);
+         return dp[m-1][n-1];
         
+     }
+    
+         
+
         
-    }
 };

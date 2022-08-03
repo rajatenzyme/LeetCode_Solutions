2ff1ac47431b2cedc06e951f4574c8c7106ea851
Rajat Mittal <rajatmittal2012@gmail.com>
2022-08-03 11:55:42 +0530

    Time: 650 ms (21.22%), Space: 18 MB (9.38%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
+    
+    int f(int i, vector<int> &nums, vector<int> &dp){
+        if(i >= nums.size()-1)
+            return 0;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        int ans = nums.size() + 1;
+        
+        for(int j = i + 1; j<= i + nums[i]; j++)
+        {
+            ans = min (ans,  1 + f(j, nums, dp) );
+        }        
+        
+        return dp[i] = ans;
+    }
+    
+    
     int jump(vector<int>& nums) {
         
-        int currReach = 0, jumps = 0, maxReach = 0;
+        int n = nums.size();
         
-        for(int i=0;i<nums.size()-1;i++){
+        vector<int> dp(n, -1);
+        
+        return f(0,nums, dp);
+        
+//         int currReach = 0, jumps = 0, maxReach = 0;
+        
+//         for(int i=0;i<nums.size()-1;i++){
             
-            if(i + nums[i] > maxReach){
-                maxReach = i + nums[i];
-            }
+//             if(i + nums[i] > maxReach){
+//                 maxReach = i + nums[i];
+//             }
          
             
-            if(i==currReach) {
-            jumps++;
-            currReach = maxReach;
-            }
+//             if(i==currReach) {
+//             jumps++;
+//             currReach = maxReach;
+//             }
+//             cout<<currReach<<" ";
             
-        }
+//         }
         
-        return jumps;
+//         return jumps;
         
     }
 };

1c47ce53091b6251d9ca38a0d010edf4b1f2bc9b
Rajat Mittal <rajatmittal2012@gmail.com>
2022-08-06 00:56:20 +0530

    Time: 6 ms (27.16%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    int noOfWays(vector<int> &nums, int target, int size, vector<int> &dp){
+    int noOfWays(vector<int> &nums, int target, int n, vector<int> &dp){
         
         if(target < 0) return 0;
         if(target == 0) return 1;
         
         if(dp[target]!=-1) return dp[target];
         
-        int ways = 0;   //NO. OF WAYS IN WHICH WE CAN FIND THE TARGET.
-        for(int i = 0; i<size; i++)
+        int ans = 0;   
+        for(int i = 0; i<n; i++)
         {
-            ways += noOfWays(nums, target - nums[i], size, dp);
+            ans += noOfWays(nums, target - nums[i], n, dp);
         }
-        return dp[target] = ways;
+        return dp[target] = ans;
         
     }
     
     
     
     int combinationSum4(vector<int>& nums, int target) {
-         vector<int> dp (target+1, -1); 
-        return noOfWays(nums, target,  nums.size(),  dp);
+        
+        int n = nums.size();
+        
+        vector<int> dp (target+1, -1); 
+        
+        
+        return noOfWays(nums, target,  n,  dp);
         
     }
 };

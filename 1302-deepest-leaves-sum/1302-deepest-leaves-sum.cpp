96fdd44e2c5cc65fbd353a27cf5dbb3003885a6b
Rajat Mittal <rajatmittal2012@gmail.com>
2022-08-07 01:04:41 +0530

    Time: 149 ms (61.09%), Space: 61.8 MB (50.13%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -37,10 +37,26 @@ public:
     
     int deepestLeavesSum(TreeNode* root) {
         
-        int h = height(root);
-        cout<<h<<endl;
+         int res = 0, i;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (q.size()) {
+            for (i = q.size() - 1, res = 0; i >= 0; --i) {
+                TreeNode* node = q.front(); q.pop();
+                res += node->val;
+                //cout<<res<<endl;
+                
+                if (node->left)  q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            cout<<res<<endl;
+        }
+        return res;
         
-        return solve(root, h-1);
+//         int h = height(root);
+//         cout<<h<<endl;
+        
+//         return solve(root, h-1);
         
         
     }

5b49a4619ef63a498306049e10c0d66374202bbe
Rajat Mittal <rajatmittal2012@gmail.com>
2022-08-07 01:09:37 +0530

    Time: 132 ms (73.60%), Space: 61.9 MB (25.76%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -12,44 +12,47 @@
 class Solution {
 public:
     
-    int height (TreeNode * root){
-        if(!root)
-            return 0;
+//     int height (TreeNode * root){
+//         if(!root)
+//             return 0;
         
-        return 1 + max(height(root->left), height(root->right));
-    }
+//         return 1 + max(height(root->left), height(root->right));
+//     }
     
-    int solve(TreeNode * root, int h){
+//     int solve(TreeNode * root, int h){
 
-        if(!root)
-            return 0;
+//         if(!root)
+//             return 0;
         
-        if(!root->left && !root->right && h==0)
-            return root->val;
+//         if(!root->left && !root->right && h==0)
+//             return root->val;
         
-         h = h  - 1;
+//          h = h  - 1;
         
-        return solve(root->left, h) + solve(root->right, h);
+//         return solve(root->left, h) + solve(root->right, h);
     
     
-    }
+//     }
     
     
     int deepestLeavesSum(TreeNode* root) {
         
-         int res = 0, i;
+        int res;
+        
         queue<TreeNode*> q;
         q.push(root);
-        while (q.size()) {
-            for (i = q.size() - 1, res = 0; i >= 0; --i) {
-                TreeNode* node = q.front(); q.pop();
+        
+        while (!q.empty()) {
+            res = 0;
+            for (int i = q.size() - 1; i >= 0; --i) {
+                TreeNode* node = q.front();
+                q.pop();
                 res += node->val;
                 //cout<<res<<endl;
-                
                 if (node->left)  q.push(node->left);
                 if (node->right) q.push(node->right);
             }
-            cout<<res<<endl;
+            //cout<<res<<endl;
         }
         return res;
         

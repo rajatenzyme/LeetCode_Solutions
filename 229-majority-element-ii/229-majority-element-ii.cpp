6f235ddea9b496689b793215efda4c6f371504aa
Rajat Mittal <rajatmittal2012@gmail.com>
2022-08-13 15:13:11 +0530

    Time: 27 ms (28.15%), Space: 15.9 MB (51.45%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        
+        vector<int> ans;
+        
+        int num1 = INT_MIN, num2 = INT_MIN, c1 = 0, c2 = 0;
+        
+        for(auto ele : nums){
+            if(ele == num1)
+                c1++;
+            else if(ele == num2)
+                c2++;
+            else if(c1 == 0){
+                num1 = ele;
+                c1 = 1;
+            }
+            else if(c2 == 0){
+                num2 = ele;
+                c2 = 1;
+             }
+            
+            else{
+                c1--;
+                c2--;
+            }     
+        }
+        
+        //cout<<num1<<" "<<num2;
+        
+        int num1_count = 0, num2_count = 0;
+        
+        for(int x : nums){
+            if(x == num1)
+                num1_count++;
+            if(x == num2)
+                num2_count++;
+        }
+        
+        if(num1_count > nums.size()/3)
+            ans.push_back(num1);
+        
+        if(num2_count > nums.size()/3)
+            ans.push_back(num2);
+
+        
+        
+//         if(num1_count > num2_count)
+//             return {num1};
+//         else if(num1_count<num2_count)
+//             return {num2};
+//         else{
+//             if(num1 != -1)
+//                 ans.push_back(num1);
+//             if(num2!=-1)
+//                 ans.push_back(num2);
+//         }
+    
+        
+        return ans;
+        
+        
+    }
+};

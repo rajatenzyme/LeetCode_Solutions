54c63ee5c5039b5cb62360f3f9bbe5368c52d330
Rajat Mittal <rajatmittal2012@gmail.com>
2022-08-18 16:45:55 +0530

    Time: 393 ms (36.59%), Space: 81.1 MB (29.25%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -18,8 +18,6 @@ public:
     
     int minSetSize(vector<int>& arr) {
         
-        // set<int> st;
-        
         int n = arr.size();
         int half = n / 2;
         
@@ -27,29 +25,17 @@ public:
         
         for(auto i : arr)
             mp[i]+=1;
-        
-        // for(auto i : mp)
-        //     cout<<i.first<<" "<<i.second<<endl;
-        
+
         priority_queue<pair<int, int>> pq;
         for(auto i : mp)
             pq.push({i.second, i.first});
-        
-        // while(!pq.empty()){
-        //     cout<<pq.top().first<<" "<<pq.top().second<<endl;
-        //     pq.pop();
-        // }
         
         int c = 0;
         while(n > half){
             pair<int, int> p = pq.top();
             pq.pop();
-            //cout<<p.first<<" "<<p.second<<endl;
             c++;
-            // st.insert(p.second);
             n-=p.first;
-     
-            
         }
         
         return c;

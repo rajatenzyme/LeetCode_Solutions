8de90399c7dd0f17abbb8955e878f92ae19a4dab
Rajat Mittal <rajatmittal2012@gmail.com>
2024-04-06 17:01:33 +0530

    Time: 15 ms (92.59%), Space: 12928000 (51.14%) - LeetHub

---
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+
+        stack <int> st;
+
+        for(int i=0;i<s.length();i++){
+            if(s[i] == '('){
+                st.push(i);
+            }
+            else if(s[i] == ')'){
+                if(!st.empty()) {
+                    st.pop(); 
+                }
+                else {
+                    s[i] = '#';
+                }
+            }
+        }
+
+
+    while(!st.empty()) {
+        s[st.top()] = '#'; 
+        st.pop();
+    }
+
+    string result = "";
+
+    for(int i = 0; i < s.length(); i++) {
+        if(s[i] != '#') { 
+            result += s[i];
+        }
+    }
+
+    return result;
+        
+    }
+};

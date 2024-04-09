aaa138ebf21646f75bc9864ca0e743525c473f35
Rajat Mittal <rajatmittal2012@gmail.com>
2024-04-10 00:07:13 +0530

    Time: 0 ms (100.00%), Space: 9340000 (67.56%) - LeetHub

---
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+
+        int ans = 0;
+        
+        while(tickets[k] != 0){
+            for(int i=0;i<tickets.size();i++){
+                if(tickets[i] == 0)
+                    continue;
+                tickets[i] --;
+                ans += 1;
+                
+                if(tickets[k] == 0)
+                    return ans;
+            }
+        }
+     return ans;
+    }
+};

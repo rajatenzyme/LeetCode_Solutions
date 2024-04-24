0751814da9e6fcc218271700e0b2e010aad3724f
Rajat Mittal <rajatmittal2012@gmail.com>
2024-04-24 23:23:29 +0530

    Time: 2 ms (50.95%), Space: 7292000 (29.12%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+
+        vector<int>F(38, 0);
+        F[0] = 0;
+        F[1] = 1;
+        F[2] = 1;
+
+        for(int i=3;i<=n;i++){
+            F[i] = F[i-1] + F[i-2] + F[i-3];
+        }
+
+        return F[n];
+        
+    }
+};

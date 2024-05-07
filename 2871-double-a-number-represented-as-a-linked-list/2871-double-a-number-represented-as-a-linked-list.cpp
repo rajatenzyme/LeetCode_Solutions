baba41acfd306df86183a6c7404bcacda1839b81
Rajat Mittal <rajatmittal2012@gmail.com>
2024-05-07 20:55:52 +0530

    Time: 174 ms (61.96%), Space: 120564000 (96.80%) - LeetHub

--- 2871-double-a-number-represented-as-a-linked-list/2871-double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/2871-double-a-number-represented-as-a-linked-list.cpp
@@ -12,16 +12,13 @@ class Solution {
 public:
     ListNode* reverseLL(ListNode* head){
         ListNode* curr = head, *prev = NULL;
-
         while(curr){
             swap(curr->next, prev);
             swap(prev, curr);
         }
-
         return prev;
 
     }
-
 
     ListNode* doubleIt(ListNode* head) {
 
@@ -35,22 +32,16 @@ public:
             temp->val =  rem;
             temp = temp->next;
         }
+        head = reverseLL(head);
 
-
-        
         if(carry!=0){
             ListNode* new_node = new ListNode();
             new_node->val = carry;
-            new_node->next = NULL;
-            head = reverseLL(head);
             new_node -> next = head;
-            return new_node;
+            head = new_node;
         }
 
-        else 
-            return reverseLL(head);
+        return head;
 
-
-        
     }
 };

5654ae72581a546b5f8efcce580dfb2c9eeadcdd
Rajat Mittal <rajatmittal2012@gmail.com>
2024-05-31 01:11:52 +0530

    Time: 6 ms (88.93%), Space: 13972000 (38.46%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int, int> mp;
+
+        for(int i=0;i<nums.size();i++){
+            int rem = target - nums[i];
+            if(mp.find(rem)!=mp.end()){
+                return {i, mp[rem]};
+            }
+            else{
+                mp[nums[i]] = i;
+            }
+        }
+
+        return {};
+        
+    }
+};
